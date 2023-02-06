e28d9134c4131323a5944003e11d1434778a93d6
shinshi <neneloophost@gmail.com>
2023-02-06 23:46:35 +0900

    リファクタリング

--- api/lib/traffic_img/download_traffic_img.py
+++ api/lib/traffic_img/download_traffic_img.py
@@ -1,9 +1,11 @@
 import requests
 
-def fetch_img(img_url):
+def fetch_img(provider: str, range: str, area: str):
+    # TODO: 他プロバイダのトラフィック画像を取得できるようにする
+    # TODO: 他の期間のトラッフィック画像を取得できるようにする
+    # TODO: 地域別のトラフィック画像を取得できるようにする
+    img_url = "https://www.jpnap.net/assets/traffic/jpnap_total_day.png"
+
     # TODO: エラー処理
     r = requests.get(img_url)
     return r.content
-
-if __name__=="__main__":
-    url = "https://www.jpnap.net/assets/traffic/jpnap_total_day.png"
